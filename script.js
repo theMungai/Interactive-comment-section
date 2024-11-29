82912762e0d72c392e14d69c67eaa1674b93133d
theMungai <mungaithesimon@gmail.com>
2024-11-29 18:04:04 +0300

    Completed first phase

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // SYNTAX : fetch().then().then().catch()
 
 function fetchJSONData(){
-
     fetch("./data.json")
     .then((res) => {
         if(!res.ok){
@@ -16,5 +15,24 @@ function fetchJSONData(){
     .catch((error) =>
     console.error("Unable to fetch data", error))
 }
-
 fetchJSONData()
+
+// Adjusting height of a text area as a user keeps on typing
+const textArea = document.querySelector(".first-reply");
+
+textArea.addEventListener("input", () => {
+    textArea.style.height = "auto";
+    textArea.style.height = (textArea.scrollHeight) + 'px'
+});
+
+
+// Showing reply section
+
+const replyingField = document.querySelector(".replying-container");
+const openReplyButton = document.querySelector(".open-reply");
+
+openReplyButton.addEventListener("click", () => {
+    replyingField.style.display = "block"
+})
+
+
