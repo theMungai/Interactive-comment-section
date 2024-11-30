5315836508510b960e30ea9f7665fa010b7b89e2
theMungai <mungaithesimon@gmail.com>
2024-11-30 15:27:38 +0300

    Modified Reply section

--- script.js
+++ script.js
@@ -19,12 +19,15 @@ fetchJSONData()
 
 
 // Adjusting height of a text area as a user keeps on typing
-const textArea = document.querySelector(".first-reply");
+const textArea = document.querySelectorAll(".js-textarea");
 
-textArea.addEventListener("input", () => {
-    textArea.style.height = "auto";
-    textArea.style.height = (textArea.scrollHeight) + 'px'
-});
+textArea.forEach((typingField) => {
+    typingField.addEventListener("input", () => {
+        typingField.style.height = "auto";
+        typingField.style.height = (typingField.scrollHeight) + 'px'
+    });
+})
+
 
 
 // Showing reply section
@@ -37,19 +40,49 @@ openReplyButton.addEventListener("click", () => {
 
 
 // Modifying vote counts
-let count = 12
-const voteCount = document.querySelector('.vote-count');
-const plusVote = document.querySelector(".js-plus-vote");
-const minusVote = document.querySelector(".js-minus-vote");
 
-plusVote.addEventListener("click", () => {
-    count = count + 1;
-    voteCount.textContent = count;
-    plusVote.disabled = true;
+const voteCount = document.querySelectorAll('.js-vote-count');
+const plusVote = document.querySelectorAll(".js-plus-vote");
+const minusVote = document.querySelectorAll(".js-minus-vote");
+
+
+plusVote.forEach((addVote) => {
+    addVote.addEventListener("click", () => {
+        let count = 12
+        voteCount.forEach((vote) => {
+            count = count + 1;
+            vote.textContent = count;
+            addVote.disabled = true;
+        })
+        
+    });
+})
+
+minusVote.forEach((reduceVote) => {
+    reduceVote.addEventListener("click", () => {
+        let count = 12
+        voteCount.forEach((vote) => {
+            count = count - 1;
+            vote.textContent = count;
+            reduceVote.disabled = true;
+        })
+        
+    });
+})
+
+
+
+
+// ======== Delete alert modal ===========
+const deleteAlert = document.querySelector(".delete-comment-alert");
+const deleteCommentButton = document.querySelector(".delete");
+const confirmDeleteButton = document.querySelector(".yes-delete");
+const cancelDeleteButton = document.querySelector(".cancel")
+
+deleteCommentButton.addEventListener("click", () => {
+    deleteAlert.style.display = "block"
 });
 
-minusVote.addEventListener("click", () => {
-    count = count - 1;
-    voteCount.textContent = count;
-    minusVote.disabled = true;
-});
+cancelDeleteButton.addEventListener("click", () => {
+    deleteAlert.style.display = "none"
+})
